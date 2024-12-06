127196e6c7458d8deecb83019f827be9de9f854f
Sergei Goncharov <blist.zheleza@gmail.com>
2024-12-06 21:07:59 +0700

    feat: add geography search field test

--- tests/AppsPage/geography.spec.ts
+++ tests/AppsPage/geography.spec.ts
@@ -6,6 +6,27 @@ test.describe('Geography selector', () => {
   test('Is visible', async ({appsPage}) => {
     await appsPage.geography.checkVisibility();
   });
-  
+
+  test('Check search inside selector', async ({appsPage}) => {
+    await appsPage.geography.clickOnGeographyInput();
+
+    const countryName = await appsPage.geography.getRandomCountry();
+
+    const testCases = [
+      countryName,
+      countryName.toLowerCase(),
+      countryName.toUpperCase(),
+      ` ${countryName}`,
+      `${countryName} `,
+      ` ${countryName} `,
+    ];
+
+    for (const testCase of testCases) {
+      await appsPage.geography.typeCountryName(testCase);
+      await appsPage.geography.checkDisplayedCountry();
+      await appsPage.geography.clearInputTextWithKeyboard();
+    }
+  });
+
 });
 
