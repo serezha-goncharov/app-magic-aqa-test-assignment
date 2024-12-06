d22f7e2c7635229adcc1cac9902db8acc5fd80c2
Sergei Goncharov <blist.zheleza@gmail.com>
2024-12-06 21:07:33 +0700

    feat: add getters for elements related to geography search field

--- components/Geography.ts
+++ components/Geography.ts
@@ -1,12 +1,32 @@
-import {Locator, Page} from "@playwright/test";
+import {expect, Locator, Page} from "@playwright/test";
+import {getRandomElement} from "@util/helper";
 
 export class Geography {
-  page: Page;
-  geographyElement: Locator;
+  constructor(private page: Page) {
+  }
 
-  constructor(page: Page) {
-    this.page = page;
-    this.geographyElement = page.locator('country-single-select');
+  // Get Elements
+  geographyElement: Locator = this.page.locator('country-single-select');
+  geographyInput: Locator = this.geographyElement.locator('div.g-selector-selected');
+  selectorPanel: Locator = this.page.locator('country-single-select-panel');
+  selectorInput: Locator = this.selectorPanel.locator('selector-search input');
+  countryPanel: Locator = this.selectorPanel
+      .locator('am-scrollbar-wrap > div.group-wrap > div')
+      .filter({has: this.page.getByText('Countries')});
+
+  countryList: string[] = [];
+  randomCountry: string;
+
+  async getCountryList() {
+    const countryNames: string[] = await this.countryPanel.locator('span').allInnerTexts();
+    this.countryList = countryNames.filter(countryName => !countryName.includes('\n'));
+    return this.countryList;
+  }
+
+  async getRandomCountry() {
+    const countryList = !!this.countryList.length ? this.countryList : await this.getCountryList();
+    this.randomCountry = await getRandomElement(countryList);
+    return this.randomCountry;
   }
 
 
