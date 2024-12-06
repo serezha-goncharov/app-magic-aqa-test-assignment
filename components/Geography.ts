4f1fc8bba99d5b739d683ba7825e996b95eb6957
Sergei Goncharov <blist.zheleza@gmail.com>
2024-12-06 17:17:03 +0700

    feat: add getter for the whole element

--- components/Geography.ts
+++ components/Geography.ts
@@ -2,9 +2,11 @@ import {Page} from "@playwright/test";
 
 export class Geography {
   page: Page;
+  geographyElement: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.geographyElement = page.locator('country-single-select');
   }
 
 }
